8767d4739713413df7b623dc13233aa2485c57b1
afriano <andresfeliperianom@gmail.com>
2025-07-05 09:26:17 -0600

    Connect mongoDB datbase

--- 01-todoApp/backend/server.js
+++ 01-todoApp/backend/server.js
@@ -6,6 +6,11 @@ require('dotenv').config();
 const app = express();
 const PORT = process.env.port || 3000;
 
+mongoose
+    .connect(process.env.MONGODB_URL)
+    .then(() => console.log('Connected to mongoDB'))
+    .catch((err) => console.log(err))
+
 app.listen(PORT, () => {
     console.log(`Running in port ${PORT}`);
 })
